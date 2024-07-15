90b25811a1d0fc294eccd431c25eaf90f32d8f55
aikhae <me@eike.in>
2024-07-15 14:38:58 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const apiUrl: string = 'http://localhost:8080/api'
+export const apiUrl: string = 'https://api.hangman.eike.in/api/'
 export const userIdKey: string = 'userId'
 export const roomCodeKey: string = 'roomCode'
 export const nicknameKey: string = 'nickname'
